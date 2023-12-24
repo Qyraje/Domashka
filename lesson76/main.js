991fc52476bbb3cce05a7ae51de6944cfff3fc04
Qyraje <151866838+Qyraje@users.noreply.github.com>
2023-12-24 17:35:31 +0300

    Add files via upload

---
+++ lesson76/main.js
@@ -0,0 +1,7 @@
+const firstName = 'Mark'
+const secondName = 'Gribinichenko'
+const age = '19 y.o'
+const height = '190cm'
+const weight = '100kg'
+
+console.log(firstName, secondName, age, height, weight)
