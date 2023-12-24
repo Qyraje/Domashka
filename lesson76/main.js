42a13f43b06d26ca4961240eb0b0f33b3795a5cc
Qyraje <151866838+Qyraje@users.noreply.github.com>
2023-12-24 18:16:00 +0300

    Delete lesson76 directory

--- lesson76/main.js
+++
@@ -1,7 +0,0 @@
-const firstName = 'Mark'
-const secondName = 'Gribinichenko'
-const age = '19 y.o'
-const height = '190cm'
-const weight = '100kg'
-
-console.log(firstName, secondName, age, height, weight)
