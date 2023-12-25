b402b41c0654902f145ba2cef4bcd13e186a97f6
Qyraje <151866838+Qyraje@users.noreply.github.com>
2023-12-25 23:53:01 +0300

    Add files via upload

---
+++ lesson81/main.js
@@ -0,0 +1,14 @@
+const massive = [1, 2, 4, 6, 3, 5, 8, 22]
+
+for (let i = 0; i < massive.length; i++) {
+    if ((massive[i] % 2) === 0) {
+        console.log(massive[i])
+    }
+}
+
+
+const massive2 = ['red', 'orange', 'yellow', 'green', 'lightBlue', 'blue', 'purple']
+
+for (let a = 6; a > -1; a--) {
+    console.log(massive2[a])
+}
